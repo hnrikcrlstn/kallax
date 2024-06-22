0a20a8389f68b077a2ee5e34b9eed77251041a06
hnrikcrlstn <henrik.carlstein@gmail.com>
2024-06-22 17:13:30 +0200

    Patch: All basic data fetched and handled. Basic formating done.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # kallax
-Board game stats
+A way to display your boardgame collection with data straight from Boardgamegeek.
+View your favourites, and get suggestions for games you haven't played in a while (provided you log your plays in BGG)
+
+# Installation
+In js/bgg.js, replace bggUserName with your username from Boardgamegeek.
--- js/bgg.js
+++ js/bgg.js
@@ -1,38 +1,112 @@
-var totalCollection = {};
+/* Change this var to your Boardgamegeek username */
+var bggUserName = 'hcarl';
+
+/* Global variables related to sending boardgamedata between functions */
+var expansions = {};
+var boardGames = {};
+var specificGame = {};
+
 const api_url = 'https://boardgamegeek.com/xmlapi2/';
+
+/* Fetch game collection when page has loaded */
 $(document).ready(function() {
     fetchGames();
-    fetchAllGames();
+    fetchAllExpansions();
 })
 
-function fetchGames() {
+/* Event listeners */
+$(document).on('click', '.game-cell', function() {
+    toggleModal($(this).attr('id'));
+});
+$(document).on('click', '.gameModal-background, .gameModal-close', function(e) {
+    if (e.target == this) {
+        resetModal();
+    }
+});
+
+/* This fetches all base game, which will be displayed in the grid */
+async function fetchGames() {
     $.ajax({
-        url: api_url + 'collection?username=hcarl&stats=1&excludesubtype=boardgameexpansion',
-        dataType: 'text',
+        url: api_url + 'collection?username=' + bggUserName + '&stats=1&excludesubtype=boardgameexpansion',
+        dataType: 'text'
     }).done(function(data, statusText, XHR) {
         if (XHR.status == 200) {
+            boardGames = $.xml2json(data);
             populateGrid($.xml2json(data));
         } else {
+            /*
+            Sometimes BGG needs to rebuild their cache with a user collection
+            This makes sure the collection is fetched without sending too many request to the BGG API 
+            */
             const timeOut = setTimeout(fetchGames, 1000)
         }
     })
 }
 
-function fetchAllGames() {
+/* To be able to shown owned expansions, a separate GET needs to fetch a users expansions */
+async function fetchAllExpansions() {
     $.ajax({
-        url: api_url + 'collection?username=hcarl&stats=1',
-        dataType: 'text',
+        url: api_url + 'collection?username=' + bggUserName + '&stats=1&subtype=boardgameexpansion',
+        dataType: 'text'
     }).done(function(data, statusText, XHR) {
         if (XHR.status == 200) {
-            totalCollection = $.xml2json(data);
+            expansions = $.xml2json(data);
         } else {
-            const timeOut = setTimeout(fetchAllGames, 1000)
+            /*
+            Sometimes BGG needs to rebuild their cache with a user collection
+            This makes sure the collection is fetched without sending too many request to the BGG API 
+            */
+            const timeOut = setTimeout(fetchAllExpansions, 1000)
         }
     })
 }
 
-function populateGrid(boardgames) {
-    boardgames.item.forEach(game => {
+/* Fetch all data on specific game */
+async function fetchSpecificGame(boardGameId) {
+    $.ajax({
+        url: api_url + '/thing?id=' + boardGameId + '&stats=1',
+        dataType: 'text'
+    }).done(function(data, statusText, XHR) {
+        if (XHR.status < 300) {
+            populateModal($.xml2json(data));
+        } else {
+            const timeOut = setTimeout(fetchSpecificGame(boardGameId), 1000);
+        }
+    })
+}
+
+async function fetchLastPlayed(gameId) {
+    try {
+        const result = $.xml2json(await $.ajax({
+            url: api_url + 'plays?username=' + bggUserName + '&stats=1&id=' + gameId,
+            type: 'GET'
+        }));
+        
+        if (result.play) {
+            return result.play.date ? result.play.date : result.play[0].date;
+        } else {
+            return '-';
+        }
+    } catch(error) {
+        console.error('Error from fetchLastPlayed: ', error);
+        throw error;
+    }
+}
+
+async function populateGrid(games) {
+    /* Create a new game cell for each game in collection */
+    console.log(games.item.length);
+    for (const game of games.item) {
+        let lastPlayed;
+        try {
+            lastPlayed = await fetchLastPlayed(game.objectid);
+        } catch {
+            console.error('Error fetching last played:', error);
+
+        }
+
+        console.log(game.name.text);
+        /* Handle single data (only 2 players, only 30 min playtimes) responses */
         let playerCount = game.stats.maxplayers == game.stats.minplayers ? game.stats.maxplayers : game.stats.minplayers + '-' + game.stats.maxplayers;
         let playTime = game.stats.minplaytime == game.stats.maxplaytime ? game.stats.minplaytime : game.stats.minplaytime + '-' + game.stats.maxplaytime;
         let userScore = game.stats.rating.value == "N/A" ? 0 : game.stats.rating.value;
@@ -40,68 +114,108 @@ function populateGrid(boardgames) {
         let gameCell = document.createElement('div');
         gameCell.className = 'game-cell';
         gameCell.id = game.objectid;
+        gameCell.setAttribute('data-user-score' , userScore == "N/A" ? 0 : userScore);
+        gameCell.setAttribute('data-last-played' , lastPlayed);
         gameCell.innerHTML = '<div class="game-name">' +
-        '<h3>' + game.name + '</h3>' + 
-        '</div>' +
-        '<div class="game-cover">' +
-        '<div class="game-score-container" style="right: 10px">'+
-        '<div class="game-bgg-score game-score" style="background-position: ' + game.stats.rating.average.value * 10 + '% bottom">' + parseFloat(game.stats.rating.average.value).toFixed(2) + '</div>' +
-        '</div>' +
-        '<div class="game-score-container" style="left: 10px">' +
-        '<div class="game-user-score game-score" style="background-position: ' + userScore * 10 + '% bottom">' + game.stats.rating.value + '</div>' +
-        '</div>' +
-        '<div class="game-cover-image" style="background-image: url(' + "'" + game.image + "'" + ')">' + 
-        '</div></div>' +
-        '<div class="game-data">' + 
-        '<div class="game-data-row">' +
-        '<div>' + playerCount + '</div>' +
-        '<div>' + playTime + ' min</div>' +
-        '</div>' +
-        '<div class="game-data-row game-header-row">' +
-        '<div>Players</div><div>Play time</div>' +
-        '</div></div>'
-        
-        $('.game-grid').append(gameCell)
-    });
-    
-    $('.game-cell').on('click', function() {
-        toggleModal($(this).attr('id'));
-    });
-    $('.gameModal-background, .gameModal-close').on('click', function(e) {
-        if (e.target == this) {
-            console.log('beep');
-            resetModal();
-        }
-    });
-
-    populateModal('1'); /* for test */
+            '<h3>' + game.name + '</h3>' + 
+            '</div>' +
+            '<div class="game-cover">' +
+            '<div class="game-score-container" style="right: 10px">'+
+            '<div class="game-bgg-score game-score" style="background-position: ' + game.stats.rating.average.value * 10 + '% bottom">' + parseFloat(game.stats.rating.average.value).toFixed(2) + '</div>' +
+            '</div>' +
+            '<div class="game-score-container" style="left: 10px">' +
+            '<div class="game-user-score game-score" style="background-position: ' + userScore * 10 + '% bottom">' + game.stats.rating.value + '</div>' +
+            '</div>' +
+            '<div class="game-cover-image" style="background-image: url(' + "'" + game.image + "'" + ')">' + 
+            '</div></div>' +
+            '<div class="game-data">' + 
+            '<div class="game-data-row">' +
+            '<div>' + playerCount + '</div>' +
+            '<div>' + playTime + ' min</div>' +
+            '</div>' +
+            '<div class="game-data-row game-header-row">' +
+            '<div>Players</div><div>Play time</div>' +
+            '</div></div>';
+        $('.game-grid').append(gameCell);
+    };
 }
 
 function toggleModal(id) {
     if ($('.gameModal-background').attr('data-visible') == 0)
-        populateModal(id);
+        fetchSpecificGame(id);
     else {
         resetModal();
     }
 }
 
-function populateModal(id) {
-    $('.gameModal-background').attr('data-visible' , 1).fadeIn('150ms');
+function populateModal(game) {
+    /* Votes is used for finding highest resulting poll */
+    let votes = 0;
+    let keywords = new Array();
+    let keywordsCounter = 0;
+    let mechanicsCounter = 0;
+    game.item.link.forEach(boardLink => {
+        if (boardLink.type == "boardgamecategory") {
+            if (keywordsCounter < 2) {
+                keywords.push(boardLink.value);
+            }
+            keywordsCounter++;
+        } else if (boardLink.type == 'boardgamemechanic'){
+            if (mechanicsCounter < 2) {
+                keywords.push(boardLink.value);
+            }
+            mechanicsCounter++;
+        }
+    }); 
+    /* Recommended player counts */
+    let bestPlayerCount = 0;
+    if (game.item.minplayers.value == game.item.maxplayers.value) {
+        $('.gameModal-players .data').text(game.item.minplayers.value);
+    } else {
+        /* If not fixed player count, fetch recommended player count */
+        game.item.poll.forEach(poll => {
+            if (poll.name == "suggested_numplayers") {
+                poll.results.forEach(result => {
+                    /* Votes for best playrange */
+                    if (result.result[0].numvotes > votes) {
+                        bestPlayerCount = result.numplayers;
+                        votes = result.result[0].numvotes;
+                    }
+                })
+            }
+        })
+        $('.gameModal-players .data').text(game.item.minplayers.value + '-' + game.item.maxplayers.value + ' (best at ' + bestPlayerCount + ')');
+    }
 
-    /* Static test data */
-    $('.gameModal-name').text('7 Wonders Duel')
-    $('.gameModal-description').text("In many ways 7 Wonders Duel resembles its parent game 7 Wonders. Over three ages, players acquire cards that provide resources or advance their military or scientific development in order to develop a civilization and complete wonders. What's different about 7 Wonders Duel is that, as the title suggests, the game is solely for two players.&amp;#10;&amp;#10;Players do not draft cards simultaneously from decks of cards, but from a display of face-down and face-up cards arranged at the start of a round. A player can take a card only if it's not covered by any others, so timing comes into play, as it can with bonus moves that allow the player to take a second card immediately. As in the original game, each acquired card can be built, discarded for coins, or used to construct a wonder. Each player also starts with four wonder cards, and the construction of a wonder provides its owner with a special ability. Only seven wonders can be built, though, so one player will end up short.&amp;#10;&amp;#10;Players can purchase resources at any time from the bank, or they can gain cards during the game that provide them with resources for future building; as they are acquired, the cost for those resources increases for the opponent, representing the owner's dominance in this area.&amp;#10;&amp;#10;You can win 7 Wonders Duel in one of three ways: each time you acquire a military card, you advance the military marker toward your opponent's capital (also giving you a bonus at certain positions). If you reach the opponent's capital, you win the game immediately. Or if you acquire six of seven different scientific symbols, you achieve scientific dominance and win immediately. If none of these situations occurs, then the player with the most points at the end of the game wins")
-    $('.gameModal-image').attr('src', 'https://cf.geekdo-images.com/zdagMskTF7wJBPjX74XsRw__original/img/Ju836WNSaW7Mab9Vjq2TJ_FqhWQ=/0x0/filters:format(jpeg)/pic2576399.jpg');
-    $('.gameModal-players .data').text("2");
-    $('.gameModal-weight .data').text("2.2");
-    $('.gameModal-time .data').text("30 min");
-    $('.gameModal-score-bgg .data').text('8.1');
-    $('.gameModal-score-player .data').text('7.75');
-    $('.gameModal-last-played .data').text('2024-06-17');
-    $('.gameModal-keywords .data').text('End Game Bonuses, Income, Layering, Market, Modular board');
+    if (game.item.minplaytime.value == game.item.maxplaytime.value) {
+        $('.gameModal-time .data').text(game.item.minplaytime.value + ' min');
+    } else {
+        $('.gameModal-time .data').text(game.item.minplaytime.value + '-' + game.item.maxplaytime.value + ' min (avg ' + game.item.playingtime.value + ' min)');
+    }
+    $('.gameModal-box a.bgg-link').attr({'href': 'https://boardgamegeek.com/boardgame/' + game.item.id , 'target': '_blank'});
+    $('.gameModal-name').text(game.item.name.length > 1 ? game.item.name[0].value : game.item.name.value );
+    $('.gameModal-description').text(decodeEntities(game.item.description));
+    $('.gameModal-image').attr('src', game.item.image);
+    $('.gameModal-weight .data').text((game.item.statistics.ratings.averageweight.value * 1).toFixed(2));
+    $('.gameModal-score-player .data').text(document.getElementById(game.item.id).getAttribute('data-user-score'));
+    $('.gameModal-score-bgg .data').text((game.item.statistics.ratings.average.value * 1).toFixed(2));
+    $('.gameModal-last-played .data').text(document.getElementById(game.item.id).getAttribute('data-last-played'));
+    $('.gameModal-keywords .data').text(keywords.join(', '));
+    $('.gameModal-background').attr('data-visible' , 1).fadeIn('150ms');
 }
+
 
 function resetModal() {
     $('.gameModal-background').attr('data-visible' , 0);
     $('.gameModal-background').fadeOut('150ms');
+    setTimeout(function() {
+        $('.gameModal-name, .gameModal-description, .gameModal-box .data').text('');
+        $('.gameModal-image').attr('src' , '');
+    }, 300);
+}
+
+function decodeEntities(encodedString) {
+    var textArea = document.createElement('textarea');
+    textArea.innerHTML = encodedString;
+    return textArea.value;
 }
